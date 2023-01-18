0868e9c3a98b3f4ba62b5dd8932b505ddecd9815
bhvoetberg <b.h.voetberg@gmail.com>
2023-01-18 21:20:57 +0100

    Redirect, logging, flashing

--- flaskenv/app.py
+++ flaskenv/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from datetime import datetime
+from logging import DEBUG
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
+app.secret_key = b'\xda\x8cGU\xe3\x00\xa4\xc0'
 
 
 # @app.route('/')
 # def home():
 #     return "Hello World "
+
+
+response = []
+
+
+def store_feedback(url):
+    response.append(dict(
+        url=url,
+        user='Me',
+        date=datetime.utcnow()
+    ))
+
+
+print(store_feedback("hello"))
+
 
 @app.route('/')
 @app.route('/index')
@@ -13,9 +32,20 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/feedback')
+@app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
+    if request.method == "POST":
+        url = request.form['url']
+        store_feedback(url)
+        app.logger.debug('stored feedback : ' + url)
+        flash("Your Feedback: " + url)
+        return redirect(url_for('index'))
     return render_template('feedback.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 # app.run()
--- readme.txt
+++ readme.txt
@@ -32,3 +32,15 @@ app.url_map
     Map([<Rule '/feedback' (HEAD, OPTIONS, GET) -> feedback>,
      <Rule '/' (HEAD, OPTIONS, GET) -> home>,
     <Rule '/static/<filename>' (HEAD, OPTIONS, GET) -> static>])
+
+
+
+___
+make key for session encoding/decoding:
+
+PyDev console: starting.
+Python 3.9.12 (main, Apr  5 2022, 06:56:58)
+[GCC 7.5.0] on linux
+import os
+os.urandom(8)
+b'\xda\x8cGU\xe3\x00\xa4\xc0'
