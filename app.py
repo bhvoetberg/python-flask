dbb953543711af405dc4695c4c46d8634cabc293
bhvoetberg <b.h.voetberg@gmail.com>
2023-02-01 21:08:42 +0100

    Completing course but database not working

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask_package import app
+from flask_package import app, db
 
 
-# app.run()
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
 # @app.route('/')
 # def home():
-#     return "Hello World "
+#     return "Hello World"
--- flask_package/forms.py
+++ flask_package/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask import flash
+from flask_package.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +11,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            flash('User already exists')
+            raise ValidationError()
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            flash('Email already exist')
+            raise ValidationError()
 
 
 class LoginForm(FlaskForm):
